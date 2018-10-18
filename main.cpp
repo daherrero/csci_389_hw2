730153c0d34616baca929d03960fbb60f569977f
mike_kalange <continuousbutnotdifferentiable@gmail.com>
2018-10-18 14:28:47 -0700

    begins working on test functions; done... space_used

--- main.cpp
+++ main.cpp
@@ -35,28 +35,35 @@ void testSet()
     // Make a cache large enough to hold 2 values;
     Cache test_cache(16);
     
+    // Initialize some keys
     const std::string key1 = "key1";
     const std::string key2 = "key2";
     const std::string key3 = "key3";
 
+    // Initialize some values
     u_int32_t const val1 = 1;
     u_int32_t const val2 = 2;
     u_int32_t const val3 = 3;
 
     Cache::val_type point1, point2, point3;
 
+    // Initialize some pointers to the values
     point1 = &val1;
     point2 = &val2;
     point3 = &val3;
     
+
+    // Test to make sure space_used function works
     u_int32_t memused = test_cache.space_used();
     std::cout << "Current memory in use: " << memused << '\n';
     std::cout << "Inserting key,value: (" << key1 << val1 << ")"<<'\n'; 
-     std::cout << "Inserting key,value: (" << key2 << val2 << ")"<<'\n';       
+    std::cout << "Inserting key,value: (" << key2 << val2 << ")"<<'\n';       
+    
     u_int32_t size = sizeof(point1);
     test_cache.set("key1",point1,size);
     test_cache.set("key2",point2,size);
 
+    memused = test_cache.space_used();
     std::cout << "Current memory in use: " << memused << '\n';
 
 
